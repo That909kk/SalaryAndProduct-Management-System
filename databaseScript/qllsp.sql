15c72c0dd87ebc95541c547f01650c631934aca8
MSI\Thanh <thanhdavid18t@gmail.com>
2023-10-26 15:11:20 +0700

    update databaseScript

--- databaseScript/qllsp.sql
+++ databaseScript/qllsp.sql
@@ -30,15 +30,17 @@ create table Xuong (
 create table CongNhan (
 	maCN nvarchar(8) not null primary key,
 	anhDaiDien varbinary(max),
-	ho nvarchar (100) not null,
+	ho nvarchar (60) not null,
 	ten nvarchar (10) not null,
 	gioiTinh bit,
+	ngaySinh date,
 	cCCD nvarchar(12) not null,
-	diaChi nvarchar (100),
-	soDienThoai nvarchar(11),
+	diaChi nvarchar (120),
+	soDienThoai nvarchar(10),
 	chuyenMon nvarchar(20),
 	caLamViec int,
 	phuCap float,
+	luongCoBan float,
 	ngayBatDauLamViec date not null,
 	maXuong nvarchar(3),
 	constraint FK_Xuong foreign key (maXuong)
@@ -47,10 +49,10 @@ create table CongNhan (
 
 create table BangLuongCongNhan (
 	maLuongCN nvarchar(12) not null primary key,
-	maCN nvarchar(8),
+	maCN nvarchar(8) not null,
 	thang int,
 	nam int,
-	sanLuong int,
+	sanLuongTong int,
 	soNgayNghiKhongPhep int,
 	tienPhat float,
 	luongTong float,
@@ -58,56 +60,59 @@ create table BangLuongCongNhan (
 	references CongNhan(maCN)
 )
 
-create table ChamCongCongNhan (
-	maCC nvarchar(12) not null primary key,
+create table BangChamCongCongNhan (
+	maCC nvarchar(13) not null primary key,
 	ngayCham date not null,
-	maCN nvarchar(8),
+	maCN nvarchar(8) not null,
+	vangMat bit,
+	coPhep bit,
 	soGioTangCa int,
-	sanLuong int,
+	sanLuong int not null,
 	ghiChu nvarchar(100),
 	constraint FK_CCCN foreign key (maCN)
 	references CongNhan(maCN)
 )
 
 create table hopDong (
-	maHopDong nvarchar(5) not null primary key,
-	tenHopDong nvarchar(20),
+	maHopDong nvarchar(6) not null primary key,
+	tenDoiTac nvarchar(30),
 	ngayThanhLyHopDong date,
 	ngayKi date
 )
 
 create table SanPham (
-	maSP nvarchar(5) not null primary key,
-	tenSP nvarchar (20) not null,
+	maSP nvarchar(11) not null primary key,
+	tenSP nvarchar (50) not null,
 	soLuong int,
 	soLuongCongDoan int,
-	maHopDong nvarchar(5),
+	maHopDong nvarchar(6) not null,
 	constraint FK_HD foreign key (maHopDong)
 	references HopDong(maHopDong)
 )
 
 create table CongDoan (
-	maCongDoan nvarchar(12) not null primary key,
+	maCongDoan nvarchar(10) not null primary key,
 	tenCongDoan nvarchar(20),
 	soLuongSanPham int not null,
 	trangThai nvarchar(12),
 	giaTien float,
 	ngayBatDau date,
+	ngayKetThucDuKien date,
 	congDoanTienQuyet nvarchar(12),
-	maSP nvarchar(5),
+	maSP nvarchar(11) not null,
 	constraint FK_SP foreign key (maSP)
 	references SanPham(maSP)
 )
 
 create table BangPhanCongCN (
 	maPCCN nvarchar(11) not null primary key,
-	trangThai nvarchar(20),
+	trangThai bit,
 	ngayPhanCong date,
 	soLuongSP int,
 	maCN nvarchar(8),
 	constraint FK_PCCN foreign key (maCN)
 	references CongNhan(maCN),
-	maCD nvarchar(12),
+	maCD nvarchar(10),
 	constraint FK_PCCD foreign key (maCD)
 	references CongDoan(maCongDoan)
 )
@@ -121,11 +126,11 @@ create table BoPhan (
 create table NhanVien (
 	maNV nvarchar(8) not null primary key,
 	anhDaiDien varbinary(max),
-	ho nvarchar (100) not null,
+	ho nvarchar (60) not null,
 	ten nvarchar (10) not null,
 	gioiTinh bit,
-	soDienThoai nvarchar(11),
-	diaChi nvarchar (100),
+	soDienThoai nvarchar(10),
+	diaChi nvarchar (120),
 	cCCD nvarchar(12) not null,
 	ngaySinh date,
 	ngayBatDauLamViec date not null,
@@ -134,14 +139,14 @@ create table NhanVien (
 	thangBacLuong int,
 	heSoLuong float,
 	phuCap float,
-	maBP nvarchar(4),
+	maBP nvarchar(4) not null,
 	constraint FK_BP foreign key (maBP)
 	references BoPhan(maBoPhan)
 )
 
 create table BangLuongNhanVien (
 	maLuongNV nvarchar(12) not null primary key,
-	maNV nvarchar(8),
+	maNV nvarchar(8) not null,
 	thang int,
 	nam int,
 	soNgayDiLam date,
@@ -152,9 +157,9 @@ create table BangLuongNhanVien (
 	references NhanVien(maNV)
 )
 
-create table ChamCongNhanVien (
+create table BangChamCongNhanVien (
 	maChamCong nvarchar(12) not null primary key,
-	maNV nvarchar(8),
+	maNV nvarchar(8) not null,
 	ngayCham date not null,
 	soGioTangCa int,
 	caLam int,
@@ -166,22 +171,23 @@ create table ChamCongNhanVien (
 	references NhanVien(maNV)
 )
 
-create table BangPhanCongXuong (
+create table BangPhanCongQLXuong (
 	maPCX nvarchar(11) not null primary key,
-	ngayNhanXuong date,
-	maXuong nvarchar(3),
+	ngayNhanXuong date not null,
+	maXuong nvarchar(3) not null,
 	constraint FK_XuongPC foreign key (maXuong)
 	references Xuong(maXuong),
-	maNV nvarchar(8),
+	maNV nvarchar(8) not null,
 	constraint FK_NVPC foreign key (maNV)
-	references NhanVien(maNV)
+	references NhanVien(maNV),
+	ngayKetThucQLXuong date
 )
 
 create table TaiKhoan (
-	maTK nvarchar (2) not null primary key,
+	maTK nvarchar (8) not null primary key,
 	taiKhoan nvarchar(20),
 	matKhau nvarchar(16),
-	maNV nvarchar(8),
+	maNV nvarchar(8) not null,
 	ngayDNCuoi date
 	constraint FK_TKNV foreign key (maNV)
 	references NhanVien(maNV)
