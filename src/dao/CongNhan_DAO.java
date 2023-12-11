e97c6f54ae5f56bdc9fedacaf636ae48ca944ec9
HKThanh <21086351.thanh@student.iuh.edu.vn>
2023-12-11 15:19:22 +0700

    Cập nhật lại congNhan_DAO

--- src/dao/CongNhan_DAO.java
+++ src/dao/CongNhan_DAO.java
@@ -502,4 +502,41 @@ public class CongNhan_DAO {
 		}
 		return dsCN;
 	}
+	//Minh Thật
+	public ArrayList<CongNhan> layDanhSachCNTheoDK(String maXuong){
+		ArrayList<CongNhan> list = new ArrayList<CongNhan>();
+		ConnectDB.getInstance();
+		Connection con = ConnectDB.getConnection();
+		String sql = "select cn.* from CongNhan cn join Xuong x on cn.maXuong=x.maXuong where x.tenXuong like ?";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, maXuong);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				CongNhan cn = new CongNhan();
+				cn.setMaCN(rs.getString(1));
+				cn.setAnhDaiDien(rs.getBytes(2));
+				cn.setHo(rs.getString(3));
+				cn.setTen(rs.getString(4));
+				cn.setGioiTinh(rs.getBoolean(5));
+				java.sql.Date ngaySinh = rs.getDate(6);
+				LocalDate NgaySinh = ngaySinh.toLocalDate();
+				cn.setcCCD(rs.getString(7));
+				cn.setDiaChi(rs.getString(8));
+				cn.setSoDienThoai(rs.getString(9));
+				cn.setChuyenMon(rs.getNString(10));
+				cn.setCaLamViec(rs.getInt(11));
+				cn.setPhuCap(rs.getDouble(12));
+				cn.setLuongCoBan(rs.getDouble(13));
+				java.sql.Date ngayBatDauLamViec = rs.getDate(14);
+				LocalDate NgayBatDauLamViec = ngayBatDauLamViec.toLocalDate();	
+				Xuong x = new Xuong(rs.getString(15));
+				cn.setXuong(x);
+				list.add(cn);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
 }
